f7e3f92c55eaa8ffe7ff688a5b5b4df83fd3cb3f
Sanjeet <sanjeet@Sanjeets-MacBook-Pro.local>
2021-04-05 18:14:43 +0530

    files updated

--- dist/index.react-native.js
+++ dist/index.react-native.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
-const RNRandomBytes = tslib_1.__importStar(require("react-native-randombytes"));
+const RNRandomBytes = tslib_1.__importStar(require("expo-random"));
 tslib_1.__exportStar(require("./index.browser"), exports);
 function randomBytes(size) {
-    return RNRandomBytes.randomBytes(size);
+    return RNRandomBytes.getRandomBytes(size);
 }
 exports.randomBytes = randomBytes;
